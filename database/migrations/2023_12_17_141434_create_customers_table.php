3813e4910291d6b5ffa6f19b1203ef147e7f612e
KiaQuest <kkh.blackcenter@yahoo.com>
2023-12-17 22:55:18 +0300

    ff

--- database/migrations/2023_12_17_141434_create_customers_table.php
+++ database/migrations/2023_12_17_141434_create_customers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('ad')->nullable();
+            $table->string('soyad')->nullable();
+            $table->string('tel')->nullable();
             $table->timestamps();
         });
     }
